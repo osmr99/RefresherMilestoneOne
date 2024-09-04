f84d97b173233b5c10edbb91001b2bba2a9aae2c
osmr99 <omarsantiagomorales@gmail.com>
2024-09-04 16:20:58 -0500

    Jumping implemented, much better rb

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,12 +3,16 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+
+    // Default gravity from Project Settings: -9.81
+    [SerializeField] GameObject hitbox;
     [SerializeField] Rigidbody rb;
     [SerializeField] int speed;
     Vector2 movement;
     [SerializeField] GameObject player;
     bool isSprinting = false;
     float rotation;
+    [SerializeField] int jumpPower;
 
 
     // Start is called before the first frame update
@@ -20,49 +24,59 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Look();
+        
     }
 
     private void FixedUpdate()
     {
+        Look();
         Movement();
     }
 
     void Movement()
     {
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.y);
         Vector3 move = transform.right * movement.x + transform.forward * movement.y;
-        var velocity = move * speed;
+        //var velocity = move * speed;
         //velocity.y = rb.velocity.y;
-        rb.velocity = velocity;
+        //rb.velocity = velocity;
+        
     }
 
     void Look()
     {
-        if (movement.x == 1)
-            rotation = 90;
-        else if (movement.x == -1)
-            rotation = 270;
-        else if (movement.y == 1)
-            rotation = 0;
-        else if (movement.y == -1)
-            rotation = 180;
-        else if (movement.x > 0 && movement.y > 0)
+        if (movement.x * speed > 0 && movement.y * speed > 0)
             rotation = 45;
-        else if (movement.x < 0 && movement.y < 0)
+        else if (movement.x * speed < 0 && movement.y * speed < 0)
             rotation = 225;
-        else if (movement.x > 0 && movement.y < 0)
+        else if (movement.x * speed > 0 && movement.y * speed < 0)
             rotation = 135;
-        else if (movement.x < 0 && movement.y > 0)
+        else if (movement.x * speed < 0 && movement.y * speed > 0)
             rotation = 315;
+        else if (movement.x >= 1)
+            rotation = 90;
+        else if (movement.x <= -1)
+            rotation = 270;
+        else if (movement.y >= 1)
+            rotation = 0;
+        else if (movement.y <= -1)
+            rotation = 180;
+
         player.transform.rotation = Quaternion.Euler(0, rotation, 0);
     }
     public void OnMovement(InputValue value)
     {
-        movement = value.Get<Vector2>();
+        movement = value.Get<Vector2>() * speed;
     }
 
     public void OnSprint(InputValue value)
     {
         isSprinting =! isSprinting;
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if(hitbox.transform.localPosition.y <= 0.315f)
+            rb.AddForce(0, jumpPower, 0, ForceMode.Impulse);
     }
 }
